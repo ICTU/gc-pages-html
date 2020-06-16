aefeebae2a8cd75816da758f349d477e5ee665fb
Tamara de Haas <info@haas-ontwerp.nl>
2020-06-16 17:14:51 +0200

    [OD] Tipgever overzicht, auteur detail [GC] Auteur detail

---
+++ sites/od/overview-tipgevers.php
@@ -0,0 +1,36 @@
+<?php
+
+$directory = $_SERVER['DOCUMENT_ROOT'];
+
+require  $directory . '/vendor/autoload.php';
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Twig\Extension\DebugExtension;
+
+use App\Twig\GroupByMonth;
+
+$loader = new FilesystemLoader([$directory . '/templates', 'templates']);
+
+$twig = new Environment($loader, ['debug' => TRUE]);
+
+$twig->addExtension(new DebugExtension);
+$twig->addExtension(new GroupByMonth);
+
+
+echo $twig->render('overview.html.twig', [
+  'site_name' => 'Optimaal Digitaal',
+  'site_slogan' => 'Verbeter spelenderwijs je (online) dienstverlening',
+  'theme' => 'od',
+  'logo' => 'img/logo/od.svg',
+  'title' => 'Evenementen',
+  'body' => 'We organiseren veel evenementen door het hele land.',
+  'overview' => 'events',
+  'modifier' => ' page--overview-archive',
+  'breadcrumb' => [
+    'page_title' => 'Evenementen',
+    'links' => [
+      'Home'
+    ]
+  ]
+]);
